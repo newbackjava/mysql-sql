7e4df812728a50e2f7bc7a4a799285bfd8d31a41
newbackjava <hera@naver.com>
2024-11-07 14:27:11 +0900

    20241107

--- 1107-sql-DML심화2-조인.sql
+++ 1107-sql-DML심화2-조인.sql
@@ -197,3 +197,80 @@ select b.no, m.*
 from bbs b
 right join member m
 on b.writer = m.id;
+
+insert into member values ('melon', '1234', 'melon', '8888');
+insert into member values ('pen', '1234', 'pen', '8888');
+select * from member;
+select count(*) from member;
+
+select m.*, b.no, b.title 
+from member m
+left join bbs b
+on m.id = b.writer;
+
+use school;
+select * from emp;
+
+select e.empno, e.ename, e.mgr, e2.ename, e2.job
+from emp e, emp e2
+where e.mgr = e2.empno;
+
+
+
+
+
+
+
+#### 조인 확인 문제
+CREATE TABLE COMPANY (
+    ID VARCHAR(50) PRIMARY KEY,
+    NAME VARCHAR(100),
+    ADDR VARCHAR(200),
+    TEL VARCHAR(20)
+);
+CREATE TABLE PRODUCT (
+    ID INT PRIMARY KEY,
+    NAME VARCHAR(50),
+    CONTENT VARCHAR(100),
+    PRICE INT,
+    COMPANY VARCHAR(50),
+    IMG VARCHAR(50),
+    FOREIGN KEY (COMPANY) REFERENCES COMPANY(ID)
+);
+
+INSERT INTO company (ID, NAME, ADDR, TEL) VALUES
+('c100', 'good', 'seoul', '011'),
+('c200', 'joa', 'busan', '012'),
+('c300', 'maria', 'ulsan', '013'),
+('c400', 'my', 'kwangju', '014');
+
+INSERT INTO PRODUCT (ID, NAME, CONTENT, PRICE, COMPANY, IMG) VALUES
+(110, 'food11', 'fun food11', 11000, NULL, '11.png'),
+(111, 'food12', 'fun food12', 12000, NULL, '12.png'),
+(100, 'food1', 'fun food1', 1000, 'c100', '1.png'),
+(101, 'food2', 'fun food2', 2000, 'c200', '2.png'),
+(102, 'food3', 'fun food3', 3000, 'c300', '3.png'),
+(103, 'food4', 'fun food4', 4000, 'c300', '4.png'),
+(104, 'food5', 'fun food5', 5000, 'c100', '5.png'),
+(105, 'food6', 'fun food6', 6000, 'c100', '6.png'),
+(106, 'food7', 'fun food7', 7000, 'c200', '7.png'),
+(107, 'food8', 'fun food8', 8000, 'c300', '8.png'),
+(108, 'food9', 'fun food9', 9000, 'c100', '9.png'),
+(109, 'food10', 'fun food10', 10000, 'c100', '10.png');
+
+select * from company;
+select * from product;
+
+
+
+
+
+
+
+-- inner join
+-- left outer join
+-- right outer join
+
+
+
+
