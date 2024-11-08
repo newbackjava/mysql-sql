4250f192d768c37c6fb2d5ce129d6c39dc37d26e
newbackjava <hera@naver.com>
2024-11-08 15:37:23 +0900

    서브쿼리

--- 1108-sql-DML심화2-서브쿼리.sql
+++ 1108-sql-DML심화2-서브쿼리.sql
@@ -183,3 +183,64 @@ where exists (select 1 from departments where deptno = 10);
 select empno, "있음" as "결과" from employees
 where exists (select 1 from departments where deptno = 100);
 
+#### 서브쿼리 & 조인 확인 문제 ####
+use school;
+show tables;
+
+create table dept2
+as select * from dept_data;
+create table emp2
+as select * from emp_data;
+
+
+select * from dept2;
+select * from emp2;
+
+
+-- 1. 최고 연봉을 받는 직원의 이름(emp)과 그의 부서명(dept) 찾기
+select e2.ename, d2.dname
+from emp2 e2, dept2 d2
+where e2.deptno = d2.deptno and
+	e2.sal = (
+		select max(sal) from emp2
+    );
+    
+-- 2. 각 부서의 평균 연봉보다 더 많은 연봉을 받는 직원들의 목록
+select deptno, avg(sal) from emp2 group by deptno;
+-- 직원들의 정보 목록: 이름, 잡, 연봉 --> 조인X
+-- 				   이름, 잡, 연봉, 부서명 --> 조인O
+select e2.ename, e2.sal, d2.dname
+from emp2 e2
+join dept2 d2
+on e2.deptno = d2.deptno
+where e2.sal > (
+	select avg(sal) from emp e1
+    where e1.deptno = e2.deptno
+);
+
+-- 3. 부서별로 최고 연봉 목록을 구하고, 
+--    이 최고 연봉과 동일한 연봉을 받는 직원의 정보 찾기
+select e2.*, d2.dname
+from emp2 e2
+join dept2 d2
+on e2.deptno = d2.deptno
+where e2.sal in ( -- 최고값, 최고값, 최고값
+	select max(sal) from emp2
+    group by deptno
+);
+-- 4. 부서 위치가 'CHICAGO'인 모든 직원들의 정보
+select e2.*, d2.loc
+from emp2 e2
+join dept2 d2
+on e2.deptno = d2.deptno
+where d2.loc = 'CHICAGO';
+
+-- 5. 모든 매니저 (MGR) 아래에서 일하는 직원들의 목록
+select e1.ename as '직원이름', e2.ename as '매니저 이름'
+from emp2 e2
+join emp2 e1
+on e2.empno = e1.mgr;
+
+
+
+
