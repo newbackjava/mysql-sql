8e743d1d277cf0d77658579ef6a57b384ae1a320
newbackjava <hera@naver.com>
2024-11-13 11:54:25 +0900

    2024-11-13

--- 1113-sql-DDL심화2-뷰.sql
+++ 1113-sql-DDL심화2-뷰.sql
@@ -72,3 +72,110 @@ INSERT INTO employees (employee_id, first_name, last_name, department_id) VALUES
 
 select * from emp_details2; 
 select count(*) from emp_details2; -- 6개로 변동이 없음.
+
+
+#### 뷰에서의 작업이 원본테이블에 영향을 미친다.
+#### 단일테이블로 생성된 뷰만 가능하다.
+#### 가상테이블인 view이 작업이 실제테이블에 반영된다.!
+
+create view emp_eng as
+select 	employee_id, first_name, last_name
+from employees
+where department_id = '1';
+
+select * from employees;
+select * from departments;
+select * from emp_eng;
+
+-- 뷰작업
+-- update
+update emp_eng 
+set last_name = 'www'
+where employee_id = 1;
+
+select * from emp_eng;
+select * from employees;
+
+-- insert
+insert into emp_eng values (10, 'jung', 'lim');
+select * from employees;
+select * from emp_eng;
+
+-- delete
+delete from emp_eng
+where employee_id = 9;
+select * from employees;
+select * from emp_eng;
+
+#### 뷰 문제풀이
+## 문제1
+-- 학생 정보 테이블 생성
+CREATE TABLE students (
+    s_id INT PRIMARY KEY,
+    s_addr VARCHAR(50),
+    s_tel VARCHAR(10),
+    tour_id VARCHAR(10)
+);
+
+-- 투어 정보 테이블 생성
+CREATE TABLE tours (
+    t_id VARCHAR(10) PRIMARY KEY,
+    t_location VARCHAR(50),
+    t_car VARCHAR(50)
+);
+
+-- 학생 정보 데이터 삽입
+INSERT INTO students (s_id, s_addr, s_tel, tour_id) VALUES 
+(1, '강남구', '011', 't1'),
+(2, '영등포구', '012', 't2'),
+(3, '강남구', '013', 't1'),
+(4, '영등포구', '014', 't2');
+
+-- 투어 정보 데이터 삽입
+INSERT INTO tours (t_id, t_location, t_car) VALUES
+('t1', '부산', 'ktx'),
+('t2', '제주도', '비행기');
+
+create table t1_table as
+	select t_id, t_location, t_car
+	from tours
+    where t_id = 't1';
+    
+select * from t1_table;
+
+## 문제2
+create view young_view
+as
+	select s_id, s_addr, t_location, t_car
+    from students s 
+    join tours t
+    on s.tour_id = t.t_id
+    where s.s_addr = '영등포구';
+
+-- drop view young_view;
+select * from young_view;
+    
+## 문제3
+create view t1_view 
+as 
+		select  s_id, s_addr, s_tel, tour_id
+        from students
+        where tour_id = 't1';
+        
+select * from t1_view;
+
+insert into t1_view values (5, '강남구', '015', 't1');
+select * from students;
+
+delete from t1_view 
+where s_id = 5;
+select * from t1_view;
+select * from students;
+
+update t1_view
+set s_tel = '333'
+where s_id = 3;
+select * from students;
+
+
+
